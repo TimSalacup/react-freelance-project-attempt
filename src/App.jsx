53a3c2c8d2abcc288b7d5b3e469dae5172e762e6
TimSalacup <timothysalacup@gmail.com>
2025-05-22 20:05:20 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import fillerLogo from "./assets/fillerLogo.png";
+
+function App() {
+  return (
+    <>
+      <nav>
+        <div className="nav__top">
+          <img src={fillerLogo} alt="cvs logo" />
+          
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default App;
